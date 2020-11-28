b27d56c7cb26f04a68bcca86ce2d03b2a71a57d2
Martin Nyaga <martin@martinnyaga.com>
2020-11-28 01:39:15 +0000

    Bootstrap gem

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in fast_statistics.gemspec
+gemspec
+
+gem "rake", "~> 12.0"
---
+++ lib/fast_statistics.rb
@@ -0,0 +1,6 @@
+require "fast_statistics/version"
+
+module FastStatistics
+  class Error < StandardError; end
+  # Your code goes here...
+end
